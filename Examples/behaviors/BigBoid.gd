4387a534c5fdc685def61b986f01ac2df405fa9a
Meemitone <99894591+Meemitone@users.noreply.github.com>
2024-01-30 13:04:47 +0000

    Update BigBoid.gd

--- Examples/behaviors/BigBoid.gd
+++ Examples/behaviors/BigBoid.gd
@@ -9,13 +9,17 @@ extends CharacterBody3D
 
 var target:Node3D
 
+#Similar to Awake
 func _ready():
 	target = get_node(target_node_path)	
+	
+#seek is the go-to behaviour, travel at max speed towards target_pos
 func seek(target_pos:Vector3):
 	var to_target = target_pos - global_position
 	var desired = to_target.normalized() * max_speed
 	return desired - velocity
 	
+#Similar to FixedUpdate
 func _physics_process(delta):
 	force = seek(target.global_position)
 	acceleration = force / mass
@@ -25,5 +29,9 @@ func _physics_process(delta):
 		look_at(position - velocity)
 		# global_transform.basis.z = velocity.normalized()
 	
+	
 	move_and_slide()
 	
+
+func _on_timer_timeout():
+	pass # Replace with function body.
